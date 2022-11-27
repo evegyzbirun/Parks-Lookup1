90cab79c5fba2e9160a9a4f6cc97284d2be13f83
evegyzbirun <zbirunzbirun@gmail.com>
2022-11-26 19:58:11 -0800

    d postgresql -T could make the error

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -5,8 +5,33 @@ class ParksController < ApplicationController
     json_response(@parks)
   end
 
+
+  def show
+    @parks = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @parks = Park.create(park_params)
+    json_response(@park)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    @park.update(park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+  end
+
   private
   def json_response(object, status = :ok)
     render json: object, status: status
+  end
+
+  def park_params
+    params.permit(:state, :park)
   end
 end
